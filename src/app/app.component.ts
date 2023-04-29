0295135d7bd4d793d627e44bd74abcff02defbdb
Chethan Srinivas Rao <chethan.srinivas_rao@siemens-healthineers.com>
2023-04-29 11:28:29 +0530

    Trix and Materials - 29 April 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    // providers: [ToolbarService, LinkService, ImageService, HtmlEditorService],
 })
 export class AppComponent {
-  title = 'LearningLogger';
+    title = 'Knowledge Logger';
+    editorElement!: any;
+
+    allSubscriptions: Subscription[] = [];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { registerLicense } from '@syncfusion/ej2-base';
 import { AppModule } from './app/app.module';
 
+registerLicense(
+    'ORg4AjUWIQA/Gnt2VFhhQlJBfVpdX2tWfFN0RnNQdV10flFFcDwsT3RfQF5jTXxbdERgUXxWc3ZUQg=='
+);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
